07bf616646efdda8134a9ae060e22085b3db6c6d
danidan2905 <danielenrriquecamacho12@gmail.com>
2024-11-03 15:36:59 -0400

    Arreglo en knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,9 +6,8 @@ const db = require('knex')({
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
+        password: process.env.DB_PASS,
         database: process.env.DB_NAME
     }
 });
-
 module.exports = db
